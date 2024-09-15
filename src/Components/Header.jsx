8256bf98a2b0e86fb26c6a13cdae747f539f5022
Benas <you@example.com>
2024-09-15 16:58:22 +0100

    added header hide/unhide functionality, adjusted header button styling

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import '../Styles/header.css'
 const header = () => {
+
+  const scrollRef = useRef(0)
+  
+  const handleScroll =()=>{
+    const scrollPos = window.scrollY
+    console.log(scrollPos, scrollRef.current);
+
+    if (scrollPos > scrollRef.current){
+      document.getElementById('header').classList.remove('unhide')
+      document.getElementById('header').classList.add('hide')
+    }
+    else{
+      document.getElementById('header').classList.remove('hide')
+      document.getElementById('header').classList.add('unhide')
+    }
+    scrollRef.current = scrollPos
+  }
+
+  useEffect(()=>{
+    window.addEventListener('scroll',handleScroll)
+    return()=>{
+      window.removeEventListener('scroll',handleScroll)
+    }
+  },[])
+
+
   return (
-    <div className='header-container'>
-      
+    <div className='header-container' id='header'>
+      <nav className='nav-container'>
+        <ul className='nav-ul'>
+          <li className='homepage-btn'>Home</li>
+          <li className='aboutpage-btn'>About</li>
+          <li className='contactpage-btn'>Contact</li>
+          <li className='deliverypage-btn'>Delivery</li>
+        </ul>
+      </nav>
     </div>
   )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../PageSections/HomeSections/Hero'
 import ProductCatalog from '../PageSections/HomeSections/ProductCatalog'
 import ProductStory from '../PageSections/HomeSections/ProductStory'
 import Delivery from '../PageSections/HomeSections/Delivery'
+import '../Styles/homepage.css'
 
 const Home = () => {
   return (
