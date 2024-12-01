381d8540d248f129bc5cad1b6dab6c50f573d050
Benas <benas.sarachovas@gmail.com>
2024-12-01 00:21:18 +0000

    adding product story element containers

--- src/PageSections/HomeSections/ProductStory.jsx
+++ src/PageSections/HomeSections/ProductStory.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import '../../Styles/productstory.css'
 
 const ProductStory = () => {
   return (
-    <div className='section'>ProductStory</div>
+    <div className='section story_container'>
+      <div className='text'>a</div>
+      <div className='text'>b</div>
+      <div className='text'>c</div>
+
+    </div>
   )
 }
 
