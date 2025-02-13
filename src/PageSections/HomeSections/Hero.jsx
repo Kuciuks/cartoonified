837c74b27eca1fa31804fb37d9ed651a0e07cb2e
Benas <benas.sarachovas@gmail.com>
2025-02-13 23:17:25 +0000

    removed unused elements, recreated a draft of figma design for hero section

--- src/PageSections/HomeSections/Hero.jsx
+++ src/PageSections/HomeSections/Hero.jsx
@@ -9,11 +9,16 @@ const Hero = () => {
             <h1>Have You Ever Been Cartoonified?</h1>
           </div>
         </div>
-        <div className='hero-image'>
-          <img className='hero-img-1' src='https://images.rawpixel.com/image_social_portrait/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDI0LTAyL2xyL3djYmZwcjlmcjYtaW1hZ2UuanBn.jpg'/>
-        </div>
-        <div>
-          carousel
+        <div className='product-showcase-container'>
+          <div className='hero-image'>
+            <img className='hero-img-1' src='https://images.rawpixel.com/image_social_portrait/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDI0LTAyL2xyL3djYmZwcjlmcjYtaW1hZ2UuanBn.jpg'/>
+          </div>
+          <div>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+          </div>
         </div>
     </div>
   )
