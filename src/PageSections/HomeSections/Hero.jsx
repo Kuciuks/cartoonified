1cfeffc9d51940464c61ea39f240187f3c9abd6c
Benas <benas.sarachovas@gmail.com>
2025-02-14 22:32:17 +0000

    working on hero carousel

--- src/PageSections/HomeSections/Hero.jsx
+++ src/PageSections/HomeSections/Hero.jsx
@@ -14,11 +14,14 @@ const Hero = () => {
             <img className='hero-img-1' src='https://images.rawpixel.com/image_social_portrait/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDI0LTAyL2xyL3djYmZwcjlmcjYtaW1hZ2UuanBn.jpg'/>
           </div>
           <div className='product-showcase'>
-            <div className='showcase showcase-1'>1</div>
-            <div className='showcase showcase-2'>2</div>
-            <div className='showcase showcase-3'>3</div>
-            <div className='showcase showcase-4'>4</div>
+            <ul>
+              <li className='showcase showcase-1'>1</li>
+              <li className='showcase showcase-2'>2</li>
+              <li className='showcase showcase-3'>3</li>
+              <li className='showcase showcase-4'>4</li>
+            </ul>
           </div>
+          
         </div>
     </div>
   )
