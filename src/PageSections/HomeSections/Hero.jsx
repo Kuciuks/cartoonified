9bbe5cc7ad3b17037aff2350ad97799517b799a4
Benas <benas.sarachovas@gmail.com>
2025-02-15 00:02:23 +0000

    used chatgbt fix solution, need to study how this works

--- src/PageSections/HomeSections/Hero.jsx
+++ src/PageSections/HomeSections/Hero.jsx
@@ -1,63 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import '../../Styles/hero.css'
-import placeholder from '../../assets/image.png'
+import React, { useEffect, useState } from "react";
+import "../../Styles/hero.css";
+import placeholder from "../../assets/image.png";
 
 const Hero = () => {
+  const [showcase, setShowcase] = useState(0);
 
-  //used to store the state if which showcase is being showned at the moment
-  const [showcase, setShowcase] = useState(0)
-
-  //used to store the showcase elements
-  const [elements,setElements] = useState([])
-
-  //grab showcase elements one they mount DOM
-  useEffect(()=>{
-    const elements = document.querySelectorAll('.showcase')
-    setElements(elements)
-  },[])
-
-
-  const moveShowcase = () => {
-    
-
-
-
-
-    setTimeout(() => {
-      
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setShowcase((prev) => (prev + 1) % 4); // Cycles between 0 -> 1 -> 2 -> 3 -> 0
     }, 3000);
-  }
 
-
-
-
-
-
-
-
-
+    return () => clearInterval(interval); // Cleanup when component unmounts
+  }, []);
 
   return (
-    <div className='section hero-section'>
-        <div className='hero-text'>
-          <div>
-            <h1>Have You Ever Been Cartoonified?</h1>
-          </div>
+    <div className="section hero-section">
+      <div className="hero-text">
+        <h1>Have You Ever Been Cartoonified?</h1>
+      </div>
+      <div className="product-showcase-container">
+        <div className="hero-image">
+          <img className="hero-img-1" src={placeholder} />
         </div>
-        <div className='product-showcase-container'>
-          <div className='hero-image'>
-            <img className='hero-img-1' src={placeholder}/>
-          </div>
-          <div className='product-showcase'>
-              <div className='showcase showcase-1'>1</div>
-              <div className='showcase showcase-2'>2</div>
-              <div className='showcase showcase-3'>3</div>
-              <div className='showcase showcase-4'>4</div>
-          </div>
-          
+        <div className="product-showcase">
+          {["1", "2", "3", "4"].map((item, index) => (
+            <div
+              key={index}
+              className={`showcase showcase-${index + 1}`}
+              style={{
+                transform: `translateX(-${showcase * 100}%)`,
+                transition: "transform 0.5s ease-in-out",
+              }}
+            >
+              {item}
+            </div>
+          ))}
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
