cb0885981f6bf0d8fdd82fc49b98a3307b2f16c9
Benas <benas.sarachovas@gmail.com>
2025-03-18 19:16:48 +0000

    added text alignment, placeholder text

--- src/PageSections/HomeSections/ProductCatalog.jsx
+++ src/PageSections/HomeSections/ProductCatalog.jsx
@@ -3,16 +3,22 @@ import '../../Styles/carousel.css'
 const ProductCatalog = () => {
   return (
     <div className='section catalog-container'>
-      
+
       <div className='item item-1'>
         <div className='pic pic-container-1'>
           <div className='star'><img /></div>
         </div>
-        <div className='txt text-1'></div>
+        <div className='txt text-1'>
+          <span className='product-name-text'>Product Name</span>
+          <span className='product-desc-text'>The description of the product</span>
+        </div>
       </div>
 
       <div className='item item-2'>
-        <div className='txt text-2'></div>
+        <div className='txt text-2'>
+          <span className='product-name-text'>Product Name</span>
+          <span className='product-desc-text'>The description of the product</span>
+        </div>
         <div className='pic pic-container-2'>
           <div className='star'><img /></div>
         </div>
@@ -22,11 +28,17 @@ const ProductCatalog = () => {
         <div className='pic pic-container-3'>
           <div className='star'><img /></div>
         </div>
-        <div className='txt text-3'></div>
+        <div className='txt text-3'>
+          <span className='product-name-text'>Product Name</span>
+          <span className='product-desc-text'>The description of the product</span>
+        </div>
       </div>
 
       <div className='item item-4'>
-        <div className='txt text-4'></div>
+        <div className='txt text-4'>
+          <span className='product-name-text'>Product Name</span>
+          <span className='product-desc-text'>The description of the product</span>
+        </div>
         <div className='pic pic-container-4'>
           <div className='star'><img /></div>
         </div>
