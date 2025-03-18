3c3976d8e667875d33de35141d372b598bcc06c8
Benas <benas.sarachovas@gmail.com>
2025-03-18 20:38:09 +0000

    removed redundant jsx code, fixed 1/4 star positioning

--- src/PageSections/HomeSections/ProductCatalog.jsx
+++ src/PageSections/HomeSections/ProductCatalog.jsx
@@ -6,55 +6,59 @@ const ProductCatalog = () => {
   return (
     <div className='section catalog-container'>
 
+
       <div className='item item-1'>
         <div className='pic pic-container-1'>
-          <div className='product-pic product-pic-1'>
-            <img/>
-          </div>
+          <img/>
         </div>
-        <span className='gap'></span>
+
+        <img className='star-img' src={starpic}/>
+
         <div className='txt text-1'>
           <span className='product-name-text'>Product Name</span>
           <span className='product-desc-text'>The description of the product</span>
         </div>
       </div>
 
+
       <div className='item item-2'>
         <div className='txt text-2'>
           <span className='product-name-text'>Product Name</span>
           <span className='product-desc-text'>The description of the product</span>
         </div>
+
         <span className='gap'></span>
+
         <div className='pic pic-container-2'>
-          <div className='product-pic product-pic-2'>
-            <img/>
-          </div>
+          <img/>
         </div>
       </div>
 
+
       <div className='item item-3'>
         <div className='pic pic-container-3'>
-          <div className='product-pic product-pic-3'>
-            <img/>
-          </div>
+          <img/>
         </div>
         <span className='gap'></span>
+
         <div className='txt text-3'>
+
           <span className='product-name-text'>Product Name</span>
           <span className='product-desc-text'>The description of the product</span>
         </div>
       </div>
+
 
       <div className='item item-4'>
         <div className='txt text-4'>
           <span className='product-name-text'>Product Name</span>
           <span className='product-desc-text'>The description of the product</span>
         </div>
+
         <span className='gap'></span>
+
         <div className='pic pic-container-4'>
-          <div className='product-pic product-pic-4'>
-            <img/>
-          </div>
+          <img/>
         </div>
       </div>
       
