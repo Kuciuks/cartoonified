daea7b5e3447225eba34ad75fa8239c8a05ed83f
Benas <benas.sarachovas@gmail.com>
2025-03-18 19:06:46 +0000

    added text area for the pictures containers

--- src/PageSections/HomeSections/ProductCatalog.jsx
+++ src/PageSections/HomeSections/ProductCatalog.jsx
@@ -3,17 +3,33 @@ import '../../Styles/carousel.css'
 const ProductCatalog = () => {
   return (
     <div className='section catalog-container'>
+      
       <div className='item item-1'>
-        <div className='pic pic-container-1'></div>
+        <div className='pic pic-container-1'>
+          <div className='star'><img /></div>
+        </div>
+        <div className='txt text-1'></div>
       </div>
+
       <div className='item item-2'>
-        <div className='pic pic-container-2'></div>
+        <div className='txt text-2'></div>
+        <div className='pic pic-container-2'>
+          <div className='star'><img /></div>
+        </div>
       </div>
+
       <div className='item item-3'>
-        <div className='pic pic-container-3'></div>
+        <div className='pic pic-container-3'>
+          <div className='star'><img /></div>
+        </div>
+        <div className='txt text-3'></div>
       </div>
+
       <div className='item item-4'>
-        <div className='pic pic-container-4'></div>
+        <div className='txt text-4'></div>
+        <div className='pic pic-container-4'>
+          <div className='star'><img /></div>
+        </div>
       </div>
       
     </div>
