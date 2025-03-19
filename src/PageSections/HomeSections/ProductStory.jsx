351476920223dd067098f47bfa6a1e78243eeb97
Benas <benas.sarachovas@gmail.com>
2025-03-19 19:36:59 +0000

    adding how-to section components and their alignments

--- src/PageSections/HomeSections/ProductStory.jsx
+++ src/PageSections/HomeSections/ProductStory.jsx
@@ -3,10 +3,19 @@ import '../../Styles/productstory.css'
 
 const ProductStory = () => {
   return (
-    <div className='section story-container'>
-      <div className='story-text'>a</div>
-      <div className='story-text'>b</div>
-      <div className='story-text'>c</div>
+    <div className='section how-to-container'>
+      <div className='ht how-to-1'>Unallocated space</div>
+      <div className='ht how-to-2'>
+        <span className='htxt ht-txt-1'>All we need is...</span>
+        <span className='htxt ht-txt-2'>A moment captured in a form of a picture</span>
+        <span className='htxt ht-txt-3'>Your pick from the design catalog</span>
+        <span className='htxt ht-txt-4'>Amd a description for adjustments</span>
+      </div>
+      <div className='ht how-to-3'>
+        <span></span>
+        <img/>
+        <span></span>
+      </div>
 
     </div>
   )
